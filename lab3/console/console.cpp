90c2a478f84dc0581323b1c9798cd0702c36a17f
Asekur <lina.rusinovich@mail.ru>
2020-10-28 20:36:18 +0300

    Added export DLL

---
+++ lab3/console/console.cpp
@@ -0,0 +1,29 @@
+﻿// console.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "windows.h"
+#include "..\\dll\\dllMain.h"
+//описание типа указателя на функцию
+typedef void (*Func)();
+
+void dynamicDLL() {
+	HMODULE hMod = LoadLibraryA("D:\\Для вот этого мусора\\github\\OSaSP\\lab3\\dll\\Debug\\dll.dll");
+	if (hMod != NULL) {
+		Func function = (Func)GetProcAddress(hMod, "changeString");
+		if (function != NULL) {
+			function();
+		}
+		FreeLibrary(hMod);
+	}
+}
+
+void staticDLL() {
+	changeString();
+}
+
+int main()
+{
+	//dynamicDLL();
+	staticDLL();
+}
---
+++ lab3/dll/dllmain.cpp
@@ -0,0 +1,27 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include "dllMain.h"
+#include <iostream>
+
+//возможность экспорта без использования def-файлов (файлы определения модуля)
+__declspec(dllexport) void changeString() {
+	std::cout << "hello";
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule, //дескриптор модуля DLL
+                       DWORD  ul_reason_for_call, //код причины вызова точки входа
+                       LPVOID lpReserved //резервация
+                     )
+{
+	//система запускает или завершает процесс или поток
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH: //динамически: lpReserved = NULL
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH: //неудачная загрузка или успешная: lpReserved = NULL
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
